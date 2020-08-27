681755d8ace633bbade473b1a6d30b67a1fd3ecf
Heitor <heitorpereira636@gmail.com>
2020-08-27 15:49:09 -0300

    base projeto

---
+++ TCC_20/app/src/main/java/br/com/tcc_20/Main.kt
@@ -0,0 +1,12 @@
+package br.com.tcc_20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
