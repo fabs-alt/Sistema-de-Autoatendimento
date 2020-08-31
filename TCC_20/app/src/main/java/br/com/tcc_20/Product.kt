00907354ad7a28655619c7bab65e6d90590c346d
Heitor <heitorpereira636@gmail.com>
2020-08-31 20:46:32 -0300

    product

---
+++ TCC_20/app/src/main/java/br/com/tcc_20/Product.kt
@@ -0,0 +1,11 @@
+package br.com.tcc_20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
