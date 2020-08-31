70ccc7aa538ccb153a42bf392a9ea8a151a20910
Heitor <heitorpereira636@gmail.com>
2020-08-31 10:39:47 -0300

    telas 2,3

---
+++ TCC_20/app/src/main/java/br/com/tcc_20/Menu.kt
@@ -0,0 +1,11 @@
+package br.com.tcc_20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
